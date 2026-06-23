df0d7d0dc7107342afca5e496a70df37005835dc
Vinayak Shenoi <vinayak.shenoi@veeva.com>
2026-06-23 09:03:52 -0500

    Update langchain

--- backend/app/engines/ai_manager/llm_provider.py
+++ backend/app/engines/ai_manager/llm_provider.py
@@ -2,7 +2,7 @@
 
 from abc import ABC, abstractmethod
 from langchain_openai import ChatOpenAI
-from langchain_community.chat_models import ChatOllama
+from langchain_ollama import ChatOllama
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 
